aaf5dcb710a940c48c9200d228a5d8c421981e44
Nathaniel <2541428@edu.vaniercollege.qc.ca>
2025-10-26 21:20:32 -0400

    tried to finish it

--- Automaton.java
+++ Automaton.java
@@ -50,33 +50,25 @@ public class Automaton
         int[] nextState = new int[state.length];
         // Naively update the state of each cell
         // based on the state of its two neighbors.
-        for(int i = 0; i < state.length; i++) {
-            int left, center, right;
-            if(i == 0) {
-                left = 0;
-            }
-            else {
-                left = state[i - 1];
-            }
-            center = state[i];
-            if(i + 1 < state.length) {
-                right = state[i + 1];
-            }
-            else {
-                right = 0;
-            }
-            nextState[i] = (left + center + right) % 2;
+        int left =0;
+        int center =state[0];
+        for(int i=0;i<state.length;i++){
+            int right= i+1<state.length ? state[i+1]:0;
+            left = center;
+            center = right;
         }
-        state = nextState;
+        }
+    public void calculateNextState()
+    { 
+        return (left + center+ right)%2;
     }
-    
-    /**
-     * Reset the automaton.
-     */
+   /**
+    * Reset the automaton.
+    */
     public void reset()
-    {
-        Arrays.fill(state, 1);
-        // Seed the automaton with a single 'on' cell.
-        state[numberOfCells / 2] = 1;
+  {
+    Arrays.fill(state, 1);
+     // Seed the automaton with a single 'on' cell.
+     state[numberOfCells / 2] = 1;
     }
 }
